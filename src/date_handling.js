d82479618019499647132680daa6259cc793333b
Shigeo Isshiki <issiki@kacsw.or.jp>
2025-08-13 21:24:24 +0900

    Update date_handling.js

--- src/date_handling.js
+++ src/date_handling.js
@@ -245,7 +245,8 @@ const convert_to_year_month = (datechar) => {
     if (datechar) { // 日付形式の文字列がある場合
         const datechar_split = date_string_split(datechar);
         if (datechar_split.year && datechar_split.month) { // 年、月の文字列がある場合
-            return datachar_char;
+            datachar_char.char = datechar_split.year + '年' + datechar_split.month + '月';
+            datachar_char.jacsw = datechar_split.year + '/' + datechar_split.month
         }
     }
     return datachar_char;
