6208f8789ee0c418f9f6ab6e743844924c611329
Shigeo Isshiki <issiki@kacsw.or.jp>
2025-08-31 23:18:35 +0900

    Update character_handling.js

--- src/character_handling.js
+++ src/character_handling.js
@@ -56,51 +56,40 @@ const convert_string_list = {
         'ウ゛': 'ヴ', 'ワ゛': 'ヷ', 'ヲ゛': 'ヺ'
     }
 };
+const build_pattern = (keys) => {
 
-const convert_to_half_width_kana = (char) => {
+    // 入力された複数のマップキーに含まれる文字列のいずれかに一致する正規表現のパターンを作成する関数
+    // （入力値）
+    // keys = 複数のマップキー
+    // （出力値） = 正規表現のパターン
+    return new RegExp([...keys()].map(char_one => char_one.replace(/[.*+?^${}()|[$\$\]]/g, '\\$&')).join('|'), 'g');
+}
+const half_width_kana_map = new Map(Object.entries(convert_string_list.half_width_kana));
+const full_width_kana_map = new Map(Object.entries(convert_string_list.full_width_kana));
+const turbidity_kana_map = new Map(Object.entries(convert_string_list.turbidity_kana));
+const half_width_kana_pattern = build_pattern(half_width_kana_map.keys());
+const full_width_kana_pattern = build_pattern(full_width_kana_map.keys());
+const turbidity_kana_pattern = build_pattern(turbidity_kana_map.keys());
+const replace_with_map = (char, pattern, map) => {
+
+    // 文字列を正規表現で表記されたパターンに一致した場合マップにある文字列に置き換える処理をする関数
+    // （入力値） = マップキー
+    // char = 文字列
+    // pattern = 正規表現のパターン
+    // map = マップ
+    // （出力値） = 置き換え後の文字列
+    return char.replace(pattern, char_one => map.get(char_one) ?? char_one);
+};
+const convert_to_half_width_kana = (() => {
 
     // 入力された文字から可能な限り半角カナ文字に変換する関数
-    // （入力値）
-    // char = 入力文字
-    // （出力値）=半角カナ文字（可能な限り）
-    if (!char) return null; // 入力文字がない場合
-    let result = '';
-    for (const char_one of char) {
-        result += convert_string_list.half_width_kana[char_one] || char_one;
-    }
-    return result;
-
-    const half_width_kana_list = { // 全角カナと半角カナの対応を定義
-        'ア': 'ｱ', 'イ': 'ｲ', 'ウ': 'ｳ', 'エ': 'ｴ', 'オ': 'ｵ',
-        'カ': 'ｶ', 'キ': 'ｷ', 'ク': 'ｸ', 'ケ': 'ｹ', 'コ': 'ｺ',
-        'サ': 'ｻ', 'シ': 'ｼ', 'ス': 'ｽ', 'セ': 'ｾ', 'ソ': 'ｿ',
-        'タ': 'ﾀ', 'チ': 'ﾁ', 'ツ': 'ﾂ', 'テ': 'ﾃ', 'ト': 'ﾄ',
-        'ナ': 'ﾅ', 'ニ': 'ﾆ', 'ヌ': 'ﾇ', 'ネ': 'ﾈ', 'ノ': 'ﾉ',
-        'ハ': 'ﾊ', 'ヒ': 'ﾋ', 'フ': 'ﾌ', 'ヘ': 'ﾍ', 'ホ': 'ﾎ',
-        'マ': 'ﾏ', 'ミ': 'ﾐ', 'ム': 'ﾑ', 'メ': 'ﾒ', 'モ': 'ﾓ',
-        'ヤ': 'ﾔ', 'ユ': 'ﾕ', 'ヨ': 'ﾖ',
-        'ラ': 'ﾗ', 'リ': 'ﾘ', 'ル': 'ﾙ', 'レ': 'ﾚ', 'ロ': 'ﾛ',
-        'ワ': 'ﾜ', 'ヲ': 'ｦ', 'ン': 'ﾝ',
-        'ガ': 'ｶﾞ', 'ギ': 'ｷﾞ', 'グ': 'ｸﾞ', 'ゲ': 'ｹﾞ', 'ゴ': 'ｺﾞ',
-        'ザ': 'ｻﾞ', 'ジ': 'ｼﾞ', 'ズ': 'ｽﾞ', 'ゼ': 'ｾﾞ', 'ゾ': 'ｿﾞ',
-        'ダ': 'ﾀﾞ', 'ヂ': 'ﾁﾞ', 'ヅ': 'ﾂﾞ', 'デ': 'ﾃﾞ', 'ド': 'ﾄﾞ',
-        'バ': 'ﾊﾞ', 'ビ': 'ﾋﾞ', 'ブ': 'ﾌﾞ', 'ベ': 'ﾍﾞ', 'ボ': 'ﾎﾞ',
-        'パ': 'ﾊﾟ', 'ピ': 'ﾋﾟ', 'プ': 'ﾌﾟ', 'ペ': 'ﾍﾟ', 'ポ': 'ﾎﾟ',
-        'ヴ': 'ｳﾞ', 'ヷ': 'ﾜﾞ', 'ヺ': 'ｦﾞ',
-        'ァ': 'ｧ', 'ィ': 'ｨ', 'ゥ': 'ｩ', 'ェ': 'ｪ', 'ォ': 'ｫ',
-        'ッ': 'ｯ', 'ャ': 'ｬ', 'ュ': 'ｭ', 'ョ': 'ｮ',
-        '゛': 'ﾞ', '゜': 'ﾟ', '　': ' '
+    // （入力値） = 文字
+    // （出力値） = 半角カナ文字（可能な限り）
+    return (char) => {
+        if (typeof char !== 'string' || char.length === 0) return ''; // 入力文字がないか、文字列ではない場合
+        return replace_with_map(char, half_width_kana_pattern, half_width_kana_map);
     };
-    let half_width_kana_list_reg = new RegExp('(' + Object.keys(half_width_kana_list).join('|') + ')', 'g');
-    if (char) { // 文字がある場合
-        const full_width_kana = convert_to_full_width_kana(char);
-        const half_with_kana = full_width_kana.replace(half_width_kana_list_reg, (char) => {
-            return half_width_kana_list[char];
-        });
-        return half_with_kana;
-    }
-    return null;
-};
+})();
 
 const convert_to_full_width_kana = (char, hiragana_sw = true) => {
 
@@ -108,47 +97,11 @@ const convert_to_full_width_kana = (char, hiragana_sw = true) => {
     // （入力値）
     // char = 入力文字、hiragana_sw = ひらがな変換の可否を選択するスイッチ（trueで変換、falseで不変換）
     // （出力値）=全角カナ文字（可能な限り）
-    const full_width_kana_list = { // 半角カナと全角カナの対応を定義
-        'ｱ': 'ア', 'ｲ': 'イ', 'ｳ': 'ウ', 'ｴ': 'エ', 'ｵ': 'オ',
-        'ｶ': 'カ', 'ｷ': 'キ', 'ｸ': 'ク', 'ｹ': 'ケ', 'ｺ': 'コ',
-        'ｻ': 'サ', 'ｼ': 'シ', 'ｽ': 'ス', 'ｾ': 'セ', 'ｿ': 'ソ',
-        'ﾀ': 'タ', 'ﾁ': 'チ', 'ﾂ': 'ツ', 'ﾃ': 'テ', 'ﾄ': 'ト',
-        'ﾅ': 'ナ', 'ﾆ': 'ニ', 'ﾇ': 'ヌ', 'ﾈ': 'ネ', 'ﾉ': 'ノ',
-        'ﾊ': 'ハ', 'ﾋ': 'ヒ', 'ﾌ': 'フ', 'ﾍ': 'ヘ', 'ﾎ': 'ホ',
-        'ﾏ': 'マ', 'ﾐ': 'ミ', 'ﾑ': 'ム', 'ﾒ': 'メ', 'ﾓ': 'モ',
-        'ﾔ': 'ヤ', 'ﾕ': 'ユ', 'ﾖ': 'ヨ',
-        'ﾗ': 'ラ', 'ﾘ': 'リ', 'ﾙ': 'ル', 'ﾚ': 'レ', 'ﾛ': 'ロ',
-        'ﾜ': 'ワ', 'ｦ': 'ヲ', 'ﾝ': 'ン',
-        'ｧ': 'ァ', 'ｨ': 'ィ', 'ｩ': 'ゥ', 'ｪ': 'ェ', 'ｫ': 'ォ',
-        'ｯ': 'ッ', 'ｬ': 'ャ', 'ｭ': 'ュ', 'ｮ': 'ョ',
-        'ﾞ': '゛', 'ﾟ': '゜', ' ': '　'
-    };
-    let full_width_kana_list_reg = new RegExp('(' + Object.keys(full_width_kana_list).join('|') + ')', 'g');
-    const turbidity_kana_list = { // 濁点・半濁点の対応を定義
-        'カ゛': 'ガ', 'キ゛': 'ギ', 'ク゛': 'グ', 'ケ゛': 'ゲ', 'コ゛': 'ゴ',
-        'サ゛': 'ザ', 'シ゛': 'ジ', 'ス゛': 'ズ', 'セ゛': 'ゼ', 'ソ゛': 'ゾ',
-        'タ゛': 'ダ', 'チ゛': 'ヂ', 'ツ゛': 'ヅ', 'テ゛': 'デ', 'ト゛': 'ド',
-        'ハ゛': 'バ', 'ヒ゛': 'ビ', 'フ゛': 'ブ', 'ヘ゛': 'ベ', 'ホ゛': 'ボ',
-        'ハ゜': 'パ', 'ヒ゜': 'ピ', 'フ゜': 'プ', 'ヘ゜': 'ペ', 'ホ゜': 'ポ',
-        'ウ゛': 'ヴ', 'ワ゛': 'ヷ', 'ヲ゛': 'ヺ'
-    };
-    let turbidity_kana_list_reg = new RegExp('(' + Object.keys(turbidity_kana_list).join('|') + ')', 'g');
-    if (char) { // 文字がある場合
-        let full_width_kana = char;
-        if (hiragana_sw) { // ひらがなをカタカタに変換する場合
-            full_width_kana = String(full_width_kana).replace(/[\u3041-\u3096]/g, (char) => {
-                return String.fromCharCode(char.charCodeAt(0) + 96);
-            }); 
-        }
-        full_width_kana = full_width_kana.replace(full_width_kana_list_reg, (char) => {
-            return full_width_kana_list[char];
-        });
-        full_width_kana = full_width_kana.replace(turbidity_kana_list_reg, (char) => {
-            return turbidity_kana_list[char];
-        });
-        return full_width_kana;
-    }
-    return null;
+    if (typeof char !== 'string' || char.length === 0) return ''; // 入力文字がないか、文字列ではない場合
+    let result = hiragana_sw ? char.replace(/[\u3041-\u3096]/g, char_one => String.fromCodePoint(char_one.charCodeAt(0) + 0x60)) : char; // 濁点と半濁点を除外して変換
+    result = replace_with_map(result, full_width_kana_pattern, full_width_kana_map);
+    result = replace_with_map(result, turbidity_kana_pattern, turbidity_kana_map);
+    return result;
 };
 
 const convert_to_hiragana = (char, check = false) => {
